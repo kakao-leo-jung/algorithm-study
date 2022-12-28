0f35fb4f9d45bc4384b61c30a5dc3896fdfd7a12
Pureun <poornnara2@gmail.com>
2022-12-28 14:08:21 +0900

    별찍기 너무재밌다

---
+++ pureun/Ex02.java
@@ -0,0 +1,62 @@
+
+public class Ex02 {
+
+	public static void main(String[] args) {	
+		
+System.out.println("*");
+
+System.out.println("**"); 
+
+System.out.println("***"); 
+
+System.out.println("****"); 
+
+System.out.println("*****");
+
+System.out.println("============");
+
+System.out.println("    *");
+
+System.out.println("   **");
+
+System.out.println("  ***");
+
+System.out.println(" ****");
+
+System.out.println("*****");
+
+System.out.println("============="); 
+
+int num = 97;
+System.out.println(2 + "*" + 1 + "=" + 2);
+
+System.out.println(2 + "*" + 2 + "=" + 2 * 2);
+
+System.out.println("2*3=6");
+
+System.out.println(2 + "*" + 4 + "=" + 8);
+
+System.out.println(2 + "*" + 5 + "=" + (2 * 5));
+
+System.out.println(2 + "*" + 6 + "=" + 12);
+
+System.out.println(2 + "*" + 7 + "=" + 14);
+
+System.out.println(2 + "*" + 8 + "=" + 16);
+
+System.out.println(2 + "*" + 9 + "=" + 18);
+
+System.out.println("=============");
+
+System.out.println("97 * 1 = " + num*1);
+
+System.out.println("97 * 2 = " + num*2);
+
+System.out.println("97 * 3 = " + num*3);
+
+System.out.println("***");
+
+System.out.println("97 * 9 = " + num*9);
+	}
+
+}
