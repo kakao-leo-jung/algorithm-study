87172666de6bb1e96d98405363ca0ddc48ffdd1d
Pureun <poornnara2@gmail.com>
2023-01-10 18:33:26 +0900

    feat(BOJ-120811):중앙값구하기

--- app/src/main/java/algorithm/study/pureun/programmers/Prg_120811.java
+++ app/src/main/java/algorithm/study/pureun/programmers/Prg_120811.java
@@ -11,28 +11,28 @@ public class Prg_120811 {
 
 	public static void main(String[] args) {
 		Prg_120811 sol = new Prg_120811();
-		int[] sam = new int[] {9,-1,0};
+		int[] sam = new int[] { 9, -1, 0 };
 		int ans = sol.solution(sam);
 		System.out.println(ans);
 
 	}
-	
+
 	public int solution(int[] array) {
-        int answer = 0;
-        int temp =0;
-        for(int i = 0; i < array.length; i++) {
-        	for(int j = i; j < array.length; j++) {
-        		if(array[i] > array[j]) {
-        		temp = array[i];
-        		array[i] = array[j];
-        		array[j] = temp;
-        		}
-        	}
-        }
-        
-        answer = array[array.length/2];
-        return answer;
-    }
-	//정렬 사용법 아직도 미숙함 
+		int answer = 0;
+		int temp = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+
+		answer = array[array.length / 2];
+		return answer;
+	}
+	// 정렬 사용법 아직도 미숙
 
 }
