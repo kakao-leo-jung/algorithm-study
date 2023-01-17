37776d746a1dc7745feedb21e85fbb296945ba22
Pureun <poornnara2@gmail.com>
2023-01-17 15:52:01 +0900

    feat(PRG-120899):가장 큰 수 찾기

--- app/src/main/java/algorithm/study/pureun/programmers/Prg_120850.java
+++ app/src/main/java/algorithm/study/pureun/programmers/Prg_120850.java
@@ -15,47 +15,47 @@ public class Prg_120850 {
 		System.out.println(Arrays.toString(ans));
 
 	}
-	
+
 	public int[] solution(String my_string) {
-       
-        char[] temp = new char[my_string.length()];
-        int index = 0;
-        for(int i =0; i < my_string.length(); i++) {
-        	for(int j = 48; j <=57; j++) {
-        		if(my_string.charAt(i) == j) {
-        			temp[index] = my_string.charAt(i);
-        			index++;
-        		}
-        	}
-        }
-        
-        char[] tmp = new char[index];
-        
-        for(int i =0; i < index; i++) {
-        	tmp[i] = temp[i];
-        }
-        
-        //정렬이게 아니라 0을기준으로 하는거 였음
-        for(int i = 0; i < index; i++) {
-        	for(int j =0; j <index; j++) {
-        		if(tmp[i] < tmp[j]) {
-        			char ch =0;
-        			ch = tmp[i];
-        			tmp[i] = tmp[j];
-        			tmp[j] = ch;
-        		}
-        	}
-        }
-        
-        String ans = String.valueOf(tmp);
-        
-        int[] answer = new int[index];
-        
-        for(int i =0; i < index; i++) {
-        	answer[i] = ans.charAt(i) - '0';
-        }
-        
-        return answer;
-    }
+
+		char[] temp = new char[my_string.length()];
+		int index = 0;
+		for (int i = 0; i < my_string.length(); i++) {
+			for (int j = 48; j <= 57; j++) {
+				if (my_string.charAt(i) == j) {
+					temp[index] = my_string.charAt(i);
+					index++;
+				}
+			}
+		}
+
+		char[] tmp = new char[index];
+
+		for (int i = 0; i < index; i++) {
+			tmp[i] = temp[i];
+		}
+
+		// 정렬이게 아니라 0을기준으로 하는거 였음
+		for (int i = 0; i < index; i++) {
+			for (int j = 0; j < index; j++) {
+				if (tmp[i] < tmp[j]) {
+					char ch = 0;
+					ch = tmp[i];
+					tmp[i] = tmp[j];
+					tmp[j] = ch;
+				}
+			}
+		}
+
+		String ans = String.valueOf(tmp);
+
+		int[] answer = new int[index];
+
+		for (int i = 0; i < index; i++) {
+			answer[i] = ans.charAt(i) - '0';
+		}
+
+		return answer;
+	}
 
 }
---
+++ app/src/main/java/algorithm/study/pureun/programmers/Prg_120899.java
@@ -0,0 +1,37 @@
+package algorithm.study.pureun.programmers;
+
+import java.util.Arrays;
+
+/**
+ * 프로그래머스 - 가장 큰 수 찾기
+ *
+ * @link https://school.programmers.co.kr/learn/courses/30/lessons/120899
+ */
+public class Prg_120899 {
+
+	public static void main(String[] args) {
+		Prg_120899 sol = new Prg_120899();
+		int[] num = new int[] {1,8,3};
+		int[] ans = sol.solution(num);
+		System.out.println(Arrays.toString(ans));
+
+	}
+
+	public int[] solution(int[] array) {
+		int[] answer = new int[2];
+		int index =0;
+		int maxNum = 0;
+		for(int i =0; i < array.length; i++) {
+			
+			if(maxNum < array[i]) {
+				maxNum = array[i];
+				index =i;
+			}
+		}
+		
+		answer[0] = maxNum;
+		answer[1] = index;
+		return answer;
+	}
+
+}
