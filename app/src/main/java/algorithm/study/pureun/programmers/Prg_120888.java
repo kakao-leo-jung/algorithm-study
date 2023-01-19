dc3a60a07daa004bfc39e4c240e4dab3a88a453b
Pureun <poornnara2@gmail.com>
2023-01-19 18:03:34 +0900

    아직못품

---
+++ app/src/main/java/algorithm/study/pureun/programmers/Prg_120888.java
@@ -0,0 +1,25 @@
+package algorithm.study.pureun.programmers;
+
+/**
+ * 프로그래머스 - 중복된 문자 제거
+ *
+ * @link https://school.programmers.co.kr/learn/courses/30/lessons/120888
+ */
+public class Prg_120888 {
+
+	public static void main(String[] args) {
+		Prg_120888 sol = new Prg_120888();
+		String ans = sol.solution("people");
+		System.out.println(ans);
+
+	}
+	
+	public String solution(String my_string) {
+        String answer = "";
+
+      
+        
+        return sn;
+    }
+
+}
