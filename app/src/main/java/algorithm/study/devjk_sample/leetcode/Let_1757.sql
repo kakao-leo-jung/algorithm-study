7284848952a9033869035a497aff7760d1989353
DEVJK <rmsghk4254@naver.com>
2023-06-25 00:49:23 +0900

    feat(LET-1757): recyclable-and-low-fat-products / sql, select

---
+++ app/src/main/java/algorithm/study/devjk_sample/leetcode/Let_1757.sql
@@ -0,0 +1,6 @@
+/**
+ * 리트코드 - Recyclable and Low Fat Products
+ *
+ * @link https://leetcode.com/problems/recyclable-and-low-fat-products
+ */
+ select product_id from Products where low_fats = 'Y' and recyclable = 'Y;'
