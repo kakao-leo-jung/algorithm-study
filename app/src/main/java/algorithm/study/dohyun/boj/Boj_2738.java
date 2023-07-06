6493aae3fd8119bc3864eac4f76cf4a1cc76f0ae
dh0522 <gong4857@naver.com>
2023-07-06 14:09:47 +0900

    feat(Boj_2738:행렬 덧셈)

---
+++ app/src/main/java/algorithm/study/dohyun/boj/Boj_2738.java
@@ -0,0 +1,40 @@
+package algorithm.study.dohyun.boj;
+import java.util.Scanner;
+public class Boj_2738 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[][] first = new int[n][m];
+		int[][] second = new int[n][m];
+		
+		for(int i =0;i<n;i++) {
+			for(int j =0;j<m;j++) {
+				first[i][j] = sc.nextInt();
+			}
+		}
+		for(int i =0;i<n;i++) {
+			for(int j =0;j<m;j++) {
+				second[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i =0;i<n;i++) {
+			for(int j =0;j<m;j++) {
+				first[i][j] += second[i][j]; 
+			}
+		}
+		
+		for(int i =0;i<n;i++) {
+			for(int j =0;j<m;j++) {
+				System.out.print(first[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
