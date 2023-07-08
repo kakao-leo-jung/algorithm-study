38b51625e1afd2f33f07f450e279b735184ef6c1
dh0522 <gong4857@naver.com>
2023-07-08 11:51:38 +0900

    feat(Boj_2903):중앙 이동 알고리즘

---
+++ app/src/main/java/algorithm/study/dohyun/boj/Boj_2903.java
@@ -0,0 +1,26 @@
+package algorithm.study.dohyun.boj;
+import java.util.*;
+public class Boj_2903 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int tmp = 2;
+		int num = 1;
+		
+		
+		for(int i=0; i<n; i++) {
+				
+			
+			num = num*2;
+		}
+			
+		tmp = tmp + (num-1);
+			
+		System.out.println(tmp*tmp);
+		
+		
+		
+
+}}
