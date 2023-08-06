97855cdf0f08efaf296f4c45fb6dc5128d18c7e4
dh0522 <gong4857@naver.com>
2023-08-06 15:14:26 +0900

    feat(Prg_42746):가장 큰 수

--- app/src/main/java/algorithm/study/dohyun/programmers/Prg_42746.java
+++ app/src/main/java/algorithm/study/dohyun/programmers/Prg_42746.java
@@ -1,5 +1,5 @@
 package algorithm.study.dohyun.programmers;
-
+import java.util.*;
 public class Prg_42746 {
 
 	public static void main(String[] args) {
@@ -8,16 +8,22 @@ public class Prg_42746 {
 		String ans = sol.solution(num);
 		System.out.println(ans);
 	}
-	// string 으로 변환 -> 가장 앞자리 수 비교 해서 -> 붙이
-    public String solution(int[] numbers) {
-        String answer = "";
-      
-        String[] str = new String[numbers.length];
-        for(int i=0; i<numbers.length; i++) str[i] = Integer.toString(numbers[i]);	
-    
-        
-        
-        return answer;
-    }
+
+	public String solution(int[] numbers) {
+		String ans = "";
+		String[] tmp = new String[numbers.length];
+
+		for(int i=0; i< numbers.length; i++)
+			tmp[i] = numbers[i] +"";
+
+		Comparator<String> com = (s1,s2) -> (s2+s1).compareTo(s1+s2);
+		Arrays.sort(tmp,com);
+
+		for(String i: tmp)
+			ans += i;
+
+		return (ans.charAt(0) == '0') ? "0" : ans;
+
+	}
 }
 
