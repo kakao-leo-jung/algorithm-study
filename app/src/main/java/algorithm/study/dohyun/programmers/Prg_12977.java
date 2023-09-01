135816479622bfeb0f530dfb4ba69da4fbe5b3e4
dh0522 <gong4857@naver.com>
2023-09-01 21:38:22 +0900

    feat(Prg_12977):소수 만들기

--- app/src/main/java/algorithm/study/dohyun/programmers/Prg_12977.java
+++ app/src/main/java/algorithm/study/dohyun/programmers/Prg_12977.java
@@ -1,18 +1,7 @@
 package algorithm.study.dohyun.programmers;
 import java.util.*;
 public class Prg_12977 {
-    public static void main(String[] args){
 
-        Prg_12977 sol = new Prg_12977();
-        int[] nums = {1,2,3,4};
-        int ans = sol.solution(nums);
-        System.out.println(ans);
-
-        int[] nums2 = {1,2,7,6,4};
-        int ans2 = sol.solution(nums2);
-        System.out.println(ans2);
-
-    }
     private boolean prime(int num){
         for(int i =2; i<num/2; i++){
             if(num % i == 0 )
