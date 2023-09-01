9ce9e9b7b262cc107062254026f82eebce48b5f9
dh0522 <gong4857@naver.com>
2023-09-01 21:32:00 +0900

    feat(Boj_4949):균형잡힌 세상

--- app/src/main/java/algorithm/study/dohyun/boj/Boj_4949.java
+++ app/src/main/java/algorithm/study/dohyun/boj/Boj_4949.java
@@ -1,2 +1,45 @@
-package algorithm.study.dohyun.boj;public class Boj_4949 {
+package algorithm.study.dohyun.boj;
+import java.util.*;
+import java.io.*;
+import java.util.Stack;
+public class Boj_4949 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in) );
+        while(true){
+            String str = br.readLine();
+            Stack<String> stk = new Stack<>();
+
+            if(str.charAt(0) == '.')    break; // 마지막 문장이면 아웃
+
+            str = str.replace(" ","");
+
+
+            for(int i=0; i< str.length() ; i++){
+                char tmp = str.charAt(i);
+
+                if(tmp == '[') stk.push(Character.toString(tmp));
+                else if(tmp == '(') stk.push(Character.toString(tmp));
+                else if(tmp == ')'){
+                    if( stk.isEmpty() )
+                        stk.push(Character.toString(tmp));
+                    else if(stk.peek().equals("("))  stk.pop();
+                    else stk.push(Character.toString(tmp));
+                }
+                else if(tmp == ']'){
+                    if( stk.isEmpty() )
+                        stk.push(Character.toString(tmp));
+                    else if(stk.peek().equals("["))  stk.pop();
+                    else stk.push(Character.toString(tmp));
+                }
+            }
+            if( stk.isEmpty() ) System.out.println("yes");
+            else System.out.println("no");
+
+
+        }
+
+
+
+    }
 }
